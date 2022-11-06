0dc11489dead0006ecb68e73a3992f1ef8a77d63
kasiaknapowska <k.knapowska@gmail.com>
2022-11-06 16:38:21 +0100

    404 page style

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,13 +8,13 @@ import Icon404 from "../svg/icon-404.svg"
 import * as styles from "../components/index.module.css"
 
 const NotFoundPage = () => (
-  <Layout>
+ 
     <section className={styles.notFoundContainer}>
     <div className={styles.notFound}><h1>404</h1><Icon404/></div>
     <p>Ups... Strona, której szukasz, nie istnieje...</p>
     <ArrowLink text="Przejdź do strony głównej" to="/">WRÓĆ DO STRONY GŁÓWNEJ</ArrowLink>
     </section>
-  </Layout>
+  
 )
 
 export const Head = () => <Seo title="404: Not Found" />
