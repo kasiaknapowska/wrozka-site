1ba74103a91e547d76f85743e7a3e0c2b852c8bc
kasiaknapowska <k.knapowska@gmail.com>
2022-11-08 11:22:13 +0100

    informacje dla pacjenta - page content

--- src/pages/ortodoncja/informacje-dla-pacjenta.js
+++ src/pages/ortodoncja/informacje-dla-pacjenta.js
@@ -5,7 +5,6 @@ import Layout from "../../components/layout"
 import Seo from "../../components/seo"
 import Hero from "../../components/hero/Hero"
 
-
 const InformacjeDlaPacjenta = ({ location }) => (
   <Layout>
     <Hero
@@ -13,35 +12,96 @@ const InformacjeDlaPacjenta = ({ location }) => (
         type: "page",
         title: "Co warto wiedzieć?",
         description:
-          "At vero eos et accusamus et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+          "Powodzenie leczenia ortodontycznego zależne jest w znacznym stopniu od Dziecka oraz Rodziców lub Opiekunów, którzy muszą ściśle współpracować z lekarzem i stosować się do jego zaleceń. Poniżej przedstawione są podstawowe zalecenia dla Pacjenta ortodontycznego leczonego aparatem stałym oraz zdejmowanym. W dalszej części podane są informacje dotyczące retencji.",
       }}
       location={location.pathname}
     />
-    <div className="containerS">
-      <h2>Dlaczego warto leczyć zęby mleczne? </h2>
+    <section className="container">
+      <h2>Aparat stały</h2>
+      <ul>
+        <li>
+          Naklejone elementy aparatu ortodontycznego są miejscem zwiększonej
+          retencji płytki nazębnej i resztek pokarmowych. Z tego względu podczas
+          leczenia ortodontycznego należy dokładnie myć zęby szczoteczką i pastą
+          po każdym posiłku i używać płynu do płukania jamy ustnej. Należy
+          stosować środki dostępne na rynku, zawierające fluor.
+        </li>
+        <li>
+          Aparat stały jest z założenia tymczasowo naklejony na zęby pacjenta,
+          gdyż istnieje konieczność jego bezpiecznego zdjęcia po zakończeniu
+          leczenia. Dlatego należy zachować szczególną ostrożność podczas
+          jedzenia, szczególnie pokarmów twardych - należy ich unikać lub kroić
+          na małe kawałki. Nie należy też manipulować przy aparacie np.
+          długopisem, ołówkiem lub palcem.
+        </li>
+        <li>
+          Wizyty ortodontyczne odbywają się w zależności od rodzaju aparatu co
+          4- 8 tygodni
+        </li>
+      </ul>
+    </section>
+    <section className="container">
+      <h2>Aparat zdejmowany</h2>
+      <ul>
+        <li>
+          Po otrzymaniu aparatu Dziecko powinno się przyzwyczajać do jego
+          noszenia stopniowo. Przez pierwsze 3 dni należy użytkować aparat tylko
+          w ciągu dnia. Po okresie adaptacyjnym aparat powinien być noszony
+          przez min. 14 godzin na dobę (zawsze zgodnie z zaleceniami lekarza,
+          czas noszenia zależy od rodzaju aparatu)
+        </li>
+        <li>Aparat należy zdejmować podczas jedzenia i mycia zębów.</li>
+        <li>
+          Aparat należy myć po każdorazowym użyciu, a gdy nie jest używany
+          należy przechowywać go w otrzymanym pudełku..
+        </li>
+        <li>Aparat należy zabierać na wszystkie wizyty kontrolne.</li>
+      </ul>
+    </section>
+    <section className="container">
+      <h2>Retencja</h2>
       <h3>
-        Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
-        voluptatibus maiores alias consequatur aut perferendis doloribus
-        asperiores repellat.
+        Ortodoncja to nie tylko właściwe leczenie ortodontyczne. Bez utrwalenia
+        efektów leczenia ortodontycznego, zęby mogą bardzo szybko powrócić do
+        położenia sprzed leczenia.
       </h3>
       <p>
-        Et harum quidem rerum facilis est et expedita distinctio. Nam libero
-        tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo
-        minus id quod maxime placeat facere possimus, omnis voluptas assumenda
-        est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis
-        debitis aut rerum necessitatibus saepe eveniet ut et voluptates
-        repudiandae sint et molestiae non recusandae.
+        Retencja polega na ustabilizowaniu skorygowanego zgryzu i ustawienia
+        zębów. Podczas retencji stosuje się specjalne aparaty retencyjne, stałe
+        (cienki drucik przytwierdzony do wewnętrznej powierzchni zębów) lub
+        ruchome (przezroczyste nakładki na zęby).
       </p>
-      <ul>
-        <li>facilis est et expedita distinctio</li>
-        <li>aut reiciendis voluptatibus maiores</li>
-        <li>earum rerum hic tenetur a sapiente</li>
+      <p>
+        Rodzaj i czas retencji jest uzależniony od konkretnej wady zgryzu, jej
+        rodzaju , nasilenia, a także metody leczenia jaką wykorzystano, wieku
+        pacjenta, i szeregu innych czynników, na przykład genetycznych.
+      </p>
+      <h3>Zalecenia podczas retencji:</h3>
+      <ul className="twoColumns">
+        <li>
+          aparat retencyjny ruchomy powinien być noszony przez całą dobę, dopóki
+          ortodonta nie zmieni zaleceń – jest to bardzo ważne i wymaga
+          dyscypliny ze strony rodziców małego Pacjenta!
+        </li>
+        <li>
+          pamiętaj o higienie i dokładnym czyszczeniu aparatu, przy pomocy pasty
+          i szczoteczki do zębów oraz letniej (nie gorącej) wody
+        </li>
+        <li>aparat retencyjny przechowuj w specjalnym pudełku</li>
+        <li>zabieraj aparat na wszystkie wizyty kontrolne</li>
+        <li>
+          w przypadku zagubienia aparatu niezwłocznie zgłoś się do ortodonty
+        </li>
       </ul>
+    </section>
+    <div className="container">
       <Link to="/">WRÓĆ DO STRONY GŁÓWNEJ</Link>
     </div>
   </Layout>
 )
 
-export const Head = () => <Seo title="Leczenie ortodontyczne, higiena i retencja" />
+export const Head = () => (
+  <Seo title="Leczenie ortodontyczne, higiena i retencja" />
+)
 
 export default InformacjeDlaPacjenta
