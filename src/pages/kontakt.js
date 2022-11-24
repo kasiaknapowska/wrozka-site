8db5b4acbc86467fb57e1ffd329dca4deef4dab9
kasiaknapowska <k.knapowska@gmail.com>
2022-11-24 15:56:24 +0100

    contact page styles

--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -1,14 +1,11 @@
 import * as React from "react"
 import { StaticImage } from "gatsby-plugin-image"
 
-
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Hero from "../components/hero/Hero"
 import Contact from "../components/contact/Contact"
 import Form from "../components/form/Form"
-
-
 
 import * as styles from "../components/index.module.css"
 
@@ -27,14 +24,14 @@ const Kontakt = ({ location }) => {
         </div>
 
         <iframe
-          title="map"
-          className={styles.mapFrame}
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1276.013024645634!2d19.006615698671002!3d50.235418255037516!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4716cef99f26498f%3A0x977f776ff387c0a3!2sDentolandia%20%7C%20Stomatologia%20Dzieci%C4%99ca!5e0!3m2!1spl!2spl!4v1666856935652!5m2!1spl!2spl"
-          width="1200"
-          height="800"
-          allowFullScreen=""
+         title="map"
+         className={styles.mapFrame}
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2552.0311919571186!2d19.004984915607523!3d50.235322179446314!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4716cef99f26498f%3A0x977f776ff387c0a3!2sDentolandia%20%7C%20Stomatologia%20Dzieci%C4%99ca!5e0!3m2!1spl!2spl!4v1669300960019!5m2!1spl!2spl"
+          width="1080"
+          height="1080"
+          allowfullscreen=""
           loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
+          referrerpolicy="no-referrer-when-downgrade"
         ></iframe>
       </section>
       <section className={styles.formSectionContainer}>
@@ -49,13 +46,20 @@ const Kontakt = ({ location }) => {
           loading="eager"
           quality={95}
           formats={["auto", "webp", "avif"]}
-          style={{position: "absolute"}}
+          style={{ position: "absolute" }}
         />
       </section>
     </Layout>
   )
 }
 
-export const Head = () => <Seo title="Kontakt" description={"Szukasz dentysty lub ortodonty dla Twojego dziecka? Zadzwoń i umów wizytę. Leczymy bezboleśnie w podtlenku azotu i narkozie."}/>
+export const Head = () => (
+  <Seo
+    title="Kontakt"
+    description={
+      "Szukasz dentysty lub ortodonty dla Twojego dziecka? Zadzwoń i umów wizytę. Leczymy bezboleśnie w podtlenku azotu i narkozie."
+    }
+  />
+)
 
 export default Kontakt
