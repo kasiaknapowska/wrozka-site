6c18ab6f07c080a77dfbf0bce5e859ab2670facd
kasiaknapowska <k.knapowska@gmail.com>
2022-11-24 14:30:44 +0100

    contact page styles fixed

--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -30,7 +30,7 @@ const Kontakt = ({ location }) => {
           title="map"
           className={styles.mapFrame}
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1276.013024645634!2d19.006615698671002!3d50.235418255037516!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4716cef99f26498f%3A0x977f776ff387c0a3!2sDentolandia%20%7C%20Stomatologia%20Dzieci%C4%99ca!5e0!3m2!1spl!2spl!4v1666856935652!5m2!1spl!2spl"
-          width="1080"
+          width="1200"
           height="800"
           allowFullScreen=""
           loading="lazy"
